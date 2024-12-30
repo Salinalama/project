426972da170ccddf31cb689e4af41f2ead2a37dc
Salinalama <tamang.salina@thamescollege.edu.np>
2024-12-30 19:42:25 +0545

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function redirecttopage(){
-    // location.href='signInpage.html';
-    window.open("signInpage.html", "_blank");
-}
-function redirectto(){
-    window.open("registerpage.html","_blank");
-}
